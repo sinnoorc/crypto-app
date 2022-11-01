8e7932329657b2b3c4659d5ad49d79d9044d6abf
sinnoorc <sinnoorc@gmail.com>
2022-11-01 16:31:38 +0530

    refactor: :recycle: added dynamic color

--- lib/app/theme/app_theme.dart
+++ lib/app/theme/app_theme.dart
@@ -4,12 +4,28 @@ import 'package:google_fonts/google_fonts.dart';
 class AppTheme {
   AppTheme._();
 
-  ThemeData get lightTheme => ThemeData(
-        useMaterial3: true, 
+  static final _defaultLightColorScheme = ColorScheme.fromSwatch(primarySwatch: Colors.blue);
+
+  static final _defaultDarkColorScheme =
+      ColorScheme.fromSwatch(primarySwatch: Colors.blue, brightness: Brightness.dark);
+
+static  ThemeData lightTheme(ColorScheme? lightColorScheme) => ThemeData(
+        useMaterial3: true,
         textTheme: GoogleFonts.interTextTheme().apply(
           bodyColor: Colors.black,
           displayColor: Colors.black,
         ),
         brightness: Brightness.light,
+        colorScheme: lightColorScheme ?? _defaultLightColorScheme,
+      );
+
+static  ThemeData darkTheme(ColorScheme? darkColorScheme) => ThemeData(
+        useMaterial3: true,
+        textTheme: GoogleFonts.interTextTheme().apply(
+          bodyColor: Colors.white,
+          displayColor: Colors.white,
+        ),
+        brightness: Brightness.dark,
+        colorScheme: darkColorScheme ?? _defaultDarkColorScheme,
       );
 }
